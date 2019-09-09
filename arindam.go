f4bbab638f59ca5260bec51416624e4badf11a18
Arindam Mishra <arindam5191@gmail.com>
2019-09-09 11:19:01 +0530

    Create arindam.go

---
+++ arindam.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func main() {
+	quant := 5
+	fmt.Println("Arindam Go")
+	strings.Title("head first go")
+	fmt.Println(reflect.TypeOf(quant), strings.Title("head first go"))
+}
